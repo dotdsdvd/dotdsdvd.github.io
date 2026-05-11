c8a6203c89d786de145c10ed5acd2c39204c87c5
nobody <nobody@nowhere>
2026-05-11 03:05:21 +0300

    initial commit

---
+++ test.js
@@ -0,0 +1,21 @@
+function checkRes(test){
+    var result = 0;
+    if (test.question1[1].checked) {
+        result++;
+    }
+    if (test.question2[0].checked) {
+        result++;
+    }
+    if (test.question3[0].checked) {
+        result++;
+    }
+    if (test.question4[1].checked) {
+        result++;
+    }
+    const numberInput = document.getElementById('question5');
+    const valueAsNumber = numberInput.valueAsNumber; // Returns a number or NaN
+    if (document.getElementById('amount').valueAsNumber == 8) {
+        result++;
+    }
+    test.display.value = "Result:" + result;
+}
